762833c1f54816e848ed82894cc74d1e0a9bfa18
Yetao Wu <yetaowu@YetaodeMacBook-Pro.local>
2024-05-05 14:39:33 +0800

    创建

---
+++ generator/src/main/java/com/yetao/GeneratorMain.java
@@ -0,0 +1,11 @@
+package com.yetao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorMain {
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorMain.class, args);
+    }
+}
